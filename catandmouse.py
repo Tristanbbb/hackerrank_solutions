f5eca017c1ca170439afc77ce2a6f00f61630bdf
Tristanbbb <tristan.benicourt@gmail.com>
2019-09-01 10:53:31 +0200

    Add files via upload

---
+++ catandmouse.py
@@ -0,0 +1,29 @@
+#!/bin/python
+
+import math
+import os
+import random
+import re
+import sys
+
+def catAndMouse(x, y, z):
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(raw_input())
+
+    for q_itr in xrange(q):
+        xyz = raw_input().split()
+
+        x = int(xyz[0])
+
+        y = int(xyz[1])
+
+        z = int(xyz[2])
+
+        result = catAndMouse(x, y, z)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
---
+++ jumpingonclouds.py
@@ -0,0 +1,22 @@
+#!/bin/python
+
+import math
+import os
+import random
+import re
+import sys
+
+def jumpingOnClouds(c):
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(raw_input())
+
+    c = map(int, raw_input().rstrip().split())
+
+    result = jumpingOnClouds(c)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
